f8ee9a7473e6dd0994b7727973c62f473d1b85dd
michaelstsnow <michael.st.snow@gmail.com>
2018-05-21 23:31:21 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,13 +3,8 @@ def position_taken?(board,index)
   #This method must take in both the existing board and the index that the user
   #is trying to fill if the index is already occupied it will not allow this
   position_value=board[index]
-  if
-  else
-  end
 
-  end
-
-  if position_value.include? "x" or position_value.include? "o"
+  if position_value.include? "x" or position_value.include? "o" or position_value.include? "X" or position_value.include? "O"
     return true
   else
     return false
