451d4d18a29678f160da8d79aa7c1c6619eda602
michaelstsnow <michael.st.snow@gmail.com>
2018-05-21 23:29:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
-def position_taken(board,index)
+def position_taken?(board,index)
   #This method must take in both the existing board and the index that the user
   #is trying to fill if the index is already occupied it will not allow this
   position_value=board[index].downcase
